f98c81af2c920b2f4f75ee83b26b36e6dc2ee6f9
bargavkoduri <bhargavkoduri123@gmail.com>
2023-12-27 16:29:16 +0530

    fix typo in constants

--- constants.js
+++ constants.js
@@ -1,9 +1,16 @@
 require("dotenv").config();
 const MONGO_URI = process.env.MONGO_URI
 const PORT = process.env.PORT
-const SECRET_TOKEN = process.env.SECRET_TOKEN
+const JWT_SECRET = process.env.JWT_SECRET;
 const REDIS_HOST = process.env.REDIS_HOST
 const REDIS_PORT = parseInt(process.env.REDIS_PORT)
 const REDIS_PASSWORD  = process.env.REDIS_PASSWORD
 
-module.exports = {MONGO_URI,PORT,SECRET_TOKEN,REDIS_HOST,REDIS_PASSWORD,REDIS_PORT}
+module.exports = {
+  MONGO_URI,
+  PORT,
+  JWT_SECRET,
+  REDIS_HOST,
+  REDIS_PASSWORD,
+  REDIS_PORT,
+};
